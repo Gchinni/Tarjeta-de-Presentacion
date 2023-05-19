f5198304988584b56dc85658b7ae571507f56dc8
Gchinni <guillechinni2001@gmail.com>
2023-05-19 00:07:02 -0400

    Imagenes a la carpeta Public

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ import React from "react";
 export default function Footer() {
     return(
         <div class="card-footer rounded-bottom-3">
-            <a href="https://twitter.com/GuilleChinni" target="_blank" className="logo-twitter"><img src="src/assets/twitter.png" alt="imagen-twitter" /></a>
-            <a href="https://www.instagram.com/guillechinni/" target="_blank" ><img src="src/assets/insta.png" alt="imagen-instagram" /></a>
-            <a href="https://github.com/Gchinni" target="_blank" className="logo-github"><img src="src/assets/gh.png" alt="imagen-github" /></a>
+            <a href="https://twitter.com/GuilleChinni" target="_blank" className="logo-twitter"><img src="./twitter.png" alt="imagen-twitter" /></a>
+            <a href="https://www.instagram.com/guillechinni/" target="_blank" ><img src="./insta.png" alt="imagen-instagram" /></a>
+            <a href="https://github.com/Gchinni" target="_blank" className="logo-github"><img src="./gh.png" alt="imagen-github" /></a>
         </div>
     )
 }
